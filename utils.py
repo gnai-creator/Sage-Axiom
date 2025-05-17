4a1a1b649045d30e495464e7167b2bee8baf75d1
Felipe Maya Muniz <67922105+gnai-creator@users.noreply.github.com>
2025-05-17 00:52:15 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ def temporal_symmetry_loss(self, logits_seq):
         flipped = tf.reverse(logits_seq, axis=[1])
         return tf.reduce_mean(tf.square(logits_seq - flipped))
 
-    def spatial_decay_mask(self, shape, decay_rate=0.1):
+def spatial_decay_mask(self, shape, decay_rate=0.1):
         h, w = shape[1], shape[2]
         yy, xx = tf.meshgrid(tf.range(h), tf.range(w), indexing='ij')
         dist = tf.sqrt(tf.cast(yy**2 + xx**2, tf.float32))
@@ -15,20 +15,20 @@ def temporal_symmetry_loss(self, logits_seq):
         decay = tf.expand_dims(decay, axis=-1)
         return decay
 
-    def repetition_penalty(self, logits):
+def repetition_penalty(self, logits):
         probs = tf.nn.softmax(logits, axis=-1)
         entropy = -tf.reduce_sum(probs * tf.math.log(probs + 1e-8), axis=-1)
         penalty = tf.reduce_mean(entropy)
         return 0.01 * penalty
 
-    def reverse_penalty(self, logits, expected):
+def reverse_penalty(self, logits, expected):
         probs = tf.nn.softmax(logits, axis=-1)
         expected_mask = tf.reduce_max(expected, axis=-1, keepdims=True)
         non_target = 1.0 - expected_mask
         penalty = tf.reduce_mean(probs * non_target)
         return 0.01 * penalty
 
-    def edge_alignment_penalty(self, output_probs):
+def edge_alignment_penalty(self, output_probs):
         # Penaliza se padrões encostarem nas bordas
         edge_mask = tf.ones_like(output_probs[:, :, :, :1])
         edge_mask = tf.concat([
