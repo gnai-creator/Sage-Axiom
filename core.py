28d2a14c5fe361a752356fd4e1a53553de9d59e8
Felipe Maya Muniz <67922105+gnai-creator@users.noreply.github.com>
2025-05-17 02:43:43 -0300

    Update core.py

--- core.py
+++ core.py
@@ -145,7 +145,7 @@ class SageAxiom(tf.keras.Model):
 
             pred_mask = tf.reduce_max(probs, axis=-1) > 0.5
             true_mask = tf.reduce_max(expected_broadcast, axis=-1) > 0.5
-            shape_loss = self.bounding_shape_penalty(pred_mask, true_mask) * 0.05
+            shape_loss = bounding_shape_penalty(pred_mask, true_mask) * 0.05
 
             #tf.print("bbox_penalty:", bbox_loss, "channel_gate_mean:", tf.reduce_mean(channel_gate))
 
